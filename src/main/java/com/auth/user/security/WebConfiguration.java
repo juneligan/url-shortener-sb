7cd6d6b774ec7c8f59e84d456857647e5b0f9bf1
github.com-lj <ligan.june@gmail.com>
2025-03-23 20:55:56 +0800

    Get frontend.urls from envi var

--- src/main/java/com/auth/user/security/WebConfiguration.java
+++ src/main/java/com/auth/user/security/WebConfiguration.java
@@ -1,19 +1,25 @@
 package com.auth.user.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    private final List<String> frontendUrls;
+
+    public WebConfiguration(@Value("${frontend.urls}") List<String> frontendUrls) {
+        this.frontendUrls = Collections.unmodifiableList(frontendUrls);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:63165", // update this part for localhost origins
-                        "https://june-ligan-portfolio.netlify.app/"
-                )
+                .allowedOrigins(frontendUrls.toArray(new String[0]))
                 .allowedMethods("*");
     }
 }
