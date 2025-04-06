930385cbb019a2535e80c2c438be9747861a7f1f
github.com-lj <ligan.june@gmail.com>
2025-04-06 18:08:48 +0800

    Added log file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,13 @@
-INSERT INTO db_config (name, type, value, active, deleted, created_at, updated_at)
-SELECT 'Password Attempts Label', 'PASSWORD_ATTEMPTS', '{"maxAttempts": 5, "resetMinutes": 2}', 1, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
-    WHERE NOT EXISTS (
-    SELECT 1 FROM db_config WHERE type = 'PASSWORD_ATTEMPTS'
-);
+-- INSERT INTO db_config (name, type, value, active, deleted, created_at, updated_at)
+-- SELECT 'Password Attempts Label', 'PASSWORD_ATTEMPTS', '{"maxAttempts": 5, "resetMinutes": 2}', 1, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+--     WHERE NOT EXISTS (
+--     SELECT 1 FROM db_config WHERE type = 'PASSWORD_ATTEMPTS'
+-- );
+--
+-- INSERT INTO db_config (name, type, value, active, deleted, created_at, updated_at)
+-- SELECT 'OTP Attempts Label', 'OTP_ATTEMPTS', '{"maxAttempts": 5, "resetMinutes": 15}', 1, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+--     WHERE NOT EXISTS (
+--     SELECT 1 FROM db_config WHERE type = 'OTP_ATTEMPTS'
+-- );
 
-INSERT INTO db_config (name, type, value, active, deleted, created_at, updated_at)
-SELECT 'OTP Attempts Label', 'OTP_ATTEMPTS', '{"maxAttempts": 5, "resetMinutes": 15}', 1, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
-    WHERE NOT EXISTS (
-    SELECT 1 FROM db_config WHERE type = 'OTP_ATTEMPTS'
-);
+ALTER TABLE users MODIFY phone_number VARCHAR(255) NULL;
